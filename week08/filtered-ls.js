1aafcc0141c43e0fa391a24bab840c980c39458d
dneves02 <dneves02@gmail.com>
2020-11-10 15:03:58 -0800

    filter-ls

--- week08/filtered-ls.js
+++ week08/filtered-ls.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 const filePath = process.argv[2];
+const fileType = process.argv[3];
 
-var lines =  undefined;
-var fileList = undefined;
+var file =  undefined;
+var fileList = [];
 
-function getFiles (callback) {
+function getFiles () {
    fs.readdir (filePath, function doneReading(err, fileContents) {
       if (err) {
          console.log(err);
@@ -12,18 +13,19 @@ function getFiles (callback) {
 
       for (var i = 0; i < fileContents.length; i++){
          file = fileContents[i].toString().split('.')
-         if (file[1] == 'txt'){
-            fileList.append(fileContents[i])
+         if (file[1] == fileType){
+            //fileList.push(fileContents[i])
+            console.log(file[0]+"."+file[1]);
          } 
 
       }
 
-      callback ();
+      
    })
 }
 
-function logResult () {
+/* function logResult () {
    console.log(fileList);
-}
+} */
 
-getFiles(logResult);
+getFiles();
