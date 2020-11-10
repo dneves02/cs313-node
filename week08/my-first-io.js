034767cb1dfb46ff967c9b621afbf55d8cfa4ea6
dneves02 <dneves02@gmail.com>
2020-11-10 10:04:23 -0800

    first io

--- week08/my-first-io.js
+++ week08/my-first-io.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+/* Solution 1:
 
 const buf = fs.readFileSync(process.argv[2]);
 const readMe = buf.toString().split('\n');
@@ -10,7 +11,18 @@ for (var i = 0; i < readMe.length; i++) {
 }
 count -= 1;
 
-console.log(count);
+console.log(count); */
+
+/* Solution 2:
+const fs = require('fs')
+
+    const contents = fs.readFileSync(process.argv[2])
+    const lines = contents.toString().split('\n').length - 1
+    console.log(lines) */
 
 
+const lines = fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1;
+    
+console.log(lines);
 
+    
