e2638f6859ff0aed4f4bf5d8f014ccdbf902bba9
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-12-01 23:05:52 -0800

    getRate

--- week09/public/javascripts/search.js
+++ week09/public/javascripts/search.js
@@ -1,31 +1,89 @@
-function search() {
-   // Get the value from the search box
-   var searchString = $('#txtSearch').val();
-   console.log('Searching for: ' + searchString);
- 
-   // Set up the parameters to send to the API
-   var params = { s: searchString, apikey: '66f670e1' };
- 
-   // Use jQuery to make the get request
-   $.get('https://www.omdbapi.com/', params, function(data, status) {
-     // For debugging purposes, make a note that we're back
-     console.log('Back from server with the following results:');
-     console.log(status);
-     console.log(data);
- 
-     updateResultList(data);
-   });
- }
- 
- function updateResultList(data) {
-   if (data.Search && data.Search.length > 0) {
-     var resultList = $('#ulResults');
-     resultList.empty();
-     
-     // you could use a forEach here as well...
-     for (var i = 0; i < data.Search.length; i++) {
-       var title = data.Search[i].Title;
-       resultList.append('<li><p>' + title + '</p></li>');
-     }
-   }
- }
+function getRate(){
+  // Get the values from the form
+
+  var type = $('#type').val();
+  var weight = $('#weight').val();
+  console.log('Calculating rate for parcel type: '+ type);
+  console.log('Weight: '+ weight);
+  
+  calculateCost(type, weight);
+
+}
+
+function calculateCost(type, weight) {
+  var cost = 0;
+
+  if (type === "stamped") {
+      switch (true){
+        case (weight <= 1): 
+          cost = 0.55;
+        case (weight <= 2):
+          cost = 0.7;
+        case (weight <= 3):
+          cost = 0.85;
+        case (weight <= 3.5):
+          cost = 1.0;
+      }
+      console.log('Cost: '+ cost);  
+  } else if (type === "metered") {
+      switch (true){
+        case (weight <= 1): 
+          cost = 0.5;
+        case (weight<= 2):
+          cost = 0.65;
+        case (weight <= 3):
+          cost = 0.8;
+        case (weight <= 3.5):
+          cost = 0.95;
+      } 
+      console.log('Cost: '+ cost);  
+  } else if (type === "flats") {
+      switch (true){
+        case (weight <= 1): 
+          cost = 1.0;
+        case (weight <= 2):
+          cost = 1.2;
+        case (weight <= 3):
+          cost = 1.4;
+        case (weight <= 4):
+          cost = 1.6;
+        case (weight <= 5):
+          cost = 1.8;
+        case (weight <= 6):
+          cost = 2.0;
+        case (weight <= 7):
+          cost = 2.2;
+        case (weight <= 8):
+          cost = 2.4;
+        case (weight <= 9):
+          cost = 2.6;
+        case (weight <= 10):
+          cost = 2.8;
+        case (weight <= 11):
+          cost = 3.0;
+        case (weight <= 12):
+          cost = 3.2;
+        case (weight <= 13):
+          cost = 3.4;
+      }
+      console.log('Cost: '+ cost);
+
+  } else if (type === "first-class") {
+      switch (true){
+        case (weight <= 4):
+          cost = 3.8;
+        case (weight <= 8):
+          cost = 4.6;
+        case (weight <= 12):
+          cost = 5.3;
+        case (weight <= 13):
+          cost = 5.9;
+      }
+      console.log('Cost: '+ cost);
+  }
+  console.log('Your total cost is: '+ cost);
+  $('#cost').empty();
+  $('#cost').append('<h3>$' + cost + '</h3>');
+    
+}
+
